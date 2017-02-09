489db96bef885f78babb6445dd92b44c4c950bf1
Gustavo Ingrassia <gustavo@eventbrite.com>
2017-02-09 17:51:23 -0300

    playgroud

---
+++ stingray-playground/index.js
@@ -0,0 +1,48 @@
+function showWidget(eventId) {
+    EBWidgets.createWidget({
+        widgetType: 'checkout',
+        eventId: +eventId,
+        iframeContainerId: 'checkout_widget'
+    });
+}
+
+function parseUrlParams() {
+    var match,
+        pl = /\+/g, // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function(s) {
+            return decodeURIComponent(s.replace(pl, " "));
+        },
+        query = window.location.search.substring(1);
+
+    var urlParams = {};
+    while (match = search.exec(query)) {
+        urlParams[decode(match[1])] = decode(match[2]);
+    }
+    return urlParams;
+}
+
+function downloadWidgetJs(urlParams) {
+    var widgetUrlMap = {
+        qa: 'https://www.evbqa.com/static/widgets/eb_widgets.js',
+        prod: 'https://www.eventbrite.com/static/widgets/eb_widgets.js'
+    };
+    var scriptElement = document.createElement('script');
+    scriptElement.type = 'text/javascript';
+    scriptElement.src = widgetUrlMap[urlParams.env];
+    scriptElement.addEventListener('load', showWidget.bind(null, urlParams.eid))
+    document.getElementsByTagName('head')[0].appendChild(scriptElement);
+}
+
+function initApp() {
+    var urlParams = parseUrlParams();
+    downloadWidgetJs(urlParams);
+}
+
+if (window.addEventListener) {
+    window.addEventListener('load', initApp, false);
+} else if (window.attachEvent) {
+    window.attachEvent('onload', initApp);
+} else {
+    window.onload = initApp;
+}
