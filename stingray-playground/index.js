9148e4ab984d875e10fbc110e47c5fe711e9cc8d
Gustavo Ingrassia <gustavo@eventbrite.com>
2017-06-15 20:41:30 -0300

    modal

--- stingray-playground/index.js
+++ stingray-playground/index.js
@@ -1,47 +1,71 @@
-function showWidget(eventId) {
-    EBWidgets.createWidget({
+function showWidget(eventId, modal) {
+    var options = {
         widgetType: 'checkout',
         eventId: eventId,
-        iframeContainerId: 'checkout_widget',
         onOrderComplete: function() {
             console.log('Order Completed');
         }
-    });
+    };
+
+    if (modal === 'on') {
+        options['modal'] = true;
+        options['modalTriggerElementId'] = 'modal-trigger';
+    } else {
+        options['iframeContainerId'] = 'checkout-widget';
+    }
+
+    EBWidgets.createWidget(options);
 }
 
+function buildWidgetContainer(eventId, modal) {
+    if (modal === 'on') {
+        var checkoutWidget = document.getElementById('checkout-widget')
+        var button = document.createElement('button');
+        var buttonLabel = document.createTextNode('Open modal');
+
+        button.id = 'modal-trigger';
+        button.appendChild(buttonLabel);
+        checkoutWidget.appendChild(button);
+    }
+
+    showWidget(eventId, modal);
+}
+
+function decode(value, replaceRegex) {
+    return decodeURIComponent(value.replace(replaceRegex, " "));
+};
+
 function parseUrlParams() {
-    var match,
-        urlParams = {},
-        replaceRegex = /\+/g, // Regex for replacing addition symbol with a space
-        searchRegex = /([^&=]+)=?([^&]*)/g,
-        decode = function(s) {
-            return decodeURIComponent(s.replace(replaceRegex, " "));
-        },
-        query = window.location.search.substring(1);
+    var replaceRegex = /\+/g;
+    var searchRegex = /([^&=]+)=?([^&]*)/g;
+    var query = window.location.search.substring(1);
+    var match;
+    var urlParams = {};
 
     while (match = searchRegex.exec(query)) {
-        urlParams[decode(match[1])] = decode(match[2]);
+        urlParams[decode(match[1], replaceRegex)] = decode(match[2], replaceRegex);
     }
     return urlParams;
 }
 
 function downloadWidgetJs(urlParams) {
-    var scriptElement = document.createElement('script'),
-        widgetUrlMap = {
-            dev: 'https://www.evbdev.com/static/widgets/eb_widgets.js',
-            qa: 'https://www.evbqa.com/static/widgets/eb_widgets.js',
-            prod: 'https://www.eventbrite.com/static/widgets/eb_widgets.js'
-        };
+    var scriptElement = document.createElement('script');
+    var widgetUrlMap = {
+        dev: 'https://www.evbdev.com/static/widgets/eb_widgets.js',
+        qa: 'https://www.evbqa.com/static/widgets/eb_widgets.js',
+        prod: 'https://www.eventbrite.com/static/widgets/eb_widgets.js'
+    };
 
     scriptElement.type = 'text/javascript';
     scriptElement.src = widgetUrlMap[urlParams.env];
-    scriptElement.addEventListener('load', showWidget.bind(null, urlParams.eid))
+    scriptElement.addEventListener('load', buildWidgetContainer.bind(null, urlParams.eid, urlParams.modal))
     document.getElementsByTagName('head')[0].appendChild(scriptElement);
 }
 
 function fillForm(urlParams) {
     document.getElementsByName('env')[0].value = urlParams.env;
     document.getElementsByName('eid')[0].value = urlParams.eid;
+    document.getElementsByName('modal')[0].checked = urlParams.modal === 'on';
 }
 
 function initApp() {
