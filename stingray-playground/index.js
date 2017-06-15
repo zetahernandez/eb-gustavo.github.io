53e99d1f6154fae470cc030819f0c5b67b88f288
Gustavo Ingrassia <gustavo@eventbrite.com>
2017-06-15 20:12:58 -0300

    some improvements

--- stingray-playground/index.js
+++ stingray-playground/index.js
@@ -1,4 +1,52 @@
-function showWidget(eventId) {
+const replaceRegex = /\+/g; // Regex for replacing addition symbol with a space
+const searchRegex = /([^&=]+)=?([^&]*)/g;
+const widgetUrlMap = {
+    dev: 'https://www.evbdev.com/static/widgets/eb_widgets.js',
+    qa: 'https://www.evbqa.com/static/widgets/eb_widgets.js',
+    prod: 'https://www.eventbrite.com/static/widgets/eb_widgets.js'
+};
+
+const initApp = () => {
+    let urlParams = parseUrlParams();
+
+    if (!Object.keys(urlParams).length) {
+        return;
+    }
+
+    fillForm(urlParams);
+    downloadWidgetJs(urlParams);
+};
+
+const parseUrlParams = () => {
+    let match;
+    let query = window.location.search.substring(1);
+
+    let urlParams = {};
+    while (match = searchRegex.exec(query)) {
+        urlParams[decode(match[1])] = decode(match[2]);
+    }
+    return urlParams;
+};
+
+const decode = (value) => (
+    decodeURIComponent(value.replace(replaceRegex, ' '));
+);
+
+const fillForm = (urlParams) => {
+    document.getElementsByName('env')[0].value = urlParams.env;
+    document.getElementsByName('eid')[0].value = urlParams.eid;
+};
+
+const downloadWidgetJs = (urlParams) => {
+    let scriptElement = document.createElement('script');
+
+    scriptElement.type = 'text/javascript';
+    scriptElement.src = widgetUrlMap[urlParams.env];
+    scriptElement.addEventListener('load', showWidget.bind(null, urlParams.eid))
+    document.getElementsByTagName('head')[0].appendChild(scriptElement);
+};
+
+const showWidget = (eventId) => {
     EBWidgets.createWidget({
         widgetType: 'checkout',
         eventId: eventId,
@@ -7,41 +55,7 @@ function showWidget(eventId) {
             console.log('Order Completed');
         }
     });
-}
-
-function parseUrlParams() {
-    var match,
-        pl = /\+/g, // Regex for replacing addition symbol with a space
-        search = /([^&=]+)=?([^&]*)/g,
-        decode = function(s) {
-            return decodeURIComponent(s.replace(pl, " "));
-        },
-        query = window.location.search.substring(1);
-
-    var urlParams = {};
-    while (match = search.exec(query)) {
-        urlParams[decode(match[1])] = decode(match[2]);
-    }
-    return urlParams;
-}
-
-function downloadWidgetJs(urlParams) {
-    var widgetUrlMap = {
-        dev: 'https://www.evbdev.com/static/widgets/eb_widgets.js',
-        qa: 'https://www.evbqa.com/static/widgets/eb_widgets.js',
-        prod: 'https://www.eventbrite.com/static/widgets/eb_widgets.js'
-    };
-    var scriptElement = document.createElement('script');
-    scriptElement.type = 'text/javascript';
-    scriptElement.src = widgetUrlMap[urlParams.env];
-    scriptElement.addEventListener('load', showWidget.bind(null, urlParams.eid))
-    document.getElementsByTagName('head')[0].appendChild(scriptElement);
-}
-
-function initApp() {
-    var urlParams = parseUrlParams();
-    downloadWidgetJs(urlParams);
-}
+};
 
 if (window.addEventListener) {
     window.addEventListener('load', initApp, false);
